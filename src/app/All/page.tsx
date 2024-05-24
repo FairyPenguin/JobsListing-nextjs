0347d2b232fb82e8b08e8f23cccc85de43aad137
FairyPenguin <autorun81@gmail.com>
2024-05-25 00:58:13 +0300

    View All Component

--- src/app/All/page.tsx
+++ src/app/All/page.tsx
@@ -1,20 +1,14 @@
-// ======/ Imports /====== 
+// ======/ Imports /======
 
 // CSS-Module File Import
-import styles  from "./All.module.css";
+import styles from "./All.module.css";
 
 // Main Component Exported Function
 
 export default function All() {
-
-    return (
-
-        <>
-            <h1>All Route, Generated via React-Outil</h1>
-        </>
-
-    )
-
+  return (
+    <>
+      <h1>All Route, Generated via React-Outil</h1>
+    </>
+  );
 }
-
-
---
+++ src/app/[slug]/[id]/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Id() {
+  return (
+    <>
+      <h1>Component Name</h1>
+    </>
+  );
+}
