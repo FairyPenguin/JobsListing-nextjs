5115508ee365c729cdb527177368aff8e6d14210
FairyPenguin <autorun81@gmail.com>
2024-05-25 23:56:51 +0300

    POST route handler

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -7,7 +7,15 @@
 export async function POST(request: Request) {
   const res = await request.json();
 
-  return Response.json({ res });
+  return Response.json(
+    { res },
+    {
+      headers: {
+        "Content-Type": "application/josn",
+      },
+      status: 201,
+    }
+  );
 
   //   return new Response(JSON.stringify(reqBody));
 }
